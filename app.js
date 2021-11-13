47c1e476258ebf94f9058432830ca05a49c8dc07
Burton Parker <burtonparker@gmail.com>
2021-11-13 11:44:39 -0600

    end of week 3 work

---
+++ app.js
@@ -0,0 +1,18 @@
+let myName; // declaration
+myName = "Burton"; // assignment
+let myCat = "Gato"; // initialized
+
+// let zero = 0;
+// console.log(Boolean(zero));
+// let emptyString = "";
+// console.log(Boolean(emptyString));
+// let unDefined = undefined;
+// console.log(Boolean(unDefined));
+// let nUll = null;
+// console.log(Boolean(nUll));
+// let nan = 6 / "a";
+// console.log(nan);
+
+// console.log("10" / 5);
+
+console.log(null || 0);
