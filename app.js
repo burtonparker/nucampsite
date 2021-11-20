48041c852c6c4fff8a04e94749a3739e16134280
Burton Parker <burtonparker@gmail.com>
2021-11-20 11:50:31 -0600

    edits to app.js tester file

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-let myName; // declaration
-myName = "Burton"; // assignment
-let myCat = "Gato"; // initialized
-
-// let zero = 0;
-// console.log(Boolean(zero));
-// let emptyString = "";
-// console.log(Boolean(emptyString));
-// let unDefined = undefined;
-// console.log(Boolean(unDefined));
-// let nUll = null;
-// console.log(Boolean(nUll));
-// let nan = 6 / "a";
-// console.log(nan);
-
-// console.log("10" / 5);
-
-console.log(null || 0);
+let myArr = [1,2,3,4];
+console.log(myArr);
+myArr.push(5,6,7);
+console.log(myArr);
+let newArr = myArr.pop();
+myArr.push(8,9,20);
+console.log(newArr);
+console.log(myArr);
